9cd999b5ebfef8953ec0d7fdced9db1c8989ba1b
Faizan <faizanaziz2001@gmail.com>
2023-06-09 20:14:00 +0500

    Entire structure complete

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let rules = ["Rock", "Paper", "Scissor"];
+let compCount = 0;
+let playerCount = 0;
+let totalRounds=5;
+
+let addlisteners = () => {
+    const btns = document.querySelectorAll("input");
+    btns.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            console.log(event.target.value);
+            playRound(event.target.value);
+        });
+    });
+}
 
 let getComputerChoice = () => {
     let index = Math.floor(Math.random() * 10) % rules.length;
@@ -19,46 +32,89 @@ let getHumanChoice = () => {
 let conditions = (playerselection, computerselection) => {
     if (playerselection == computerselection)
         return 0;
-    if (playerselection == "Rock" && computerselection == "Scissor" || playerselection == "Paper" && computerselection == "Rock" || playerselection == "Scissor" && computerselection == "Paper")
+    if (playerselection == "Rock" && computerselection == "Scissor" ||
+        playerselection == "Paper" && computerselection == "Rock" ||
+        playerselection == "Scissor" && computerselection == "Paper")
+        {
+        playerCount++;
         return 1;
+        }
     else
+    {
+        compCount++;
         return -1;
+    }
 }
 
-let playRound = () => {
-    let playerselection = getHumanChoice();
+
+let writeSymbol=(playerselection,computerselection)=>
+{
+    const playImg=document.querySelector('.innerlayer-1 #p-img');
+    const compImg=document.querySelector('.innerlayer-1 #c-img');
+
+    playImg.src="./images/"+playerselection.toLowerCase()+".png";
+    compImg.src="/images/"+computerselection.toLowerCase()+".png";
+}
+let writeScore=(score1,score2)=>{
+    const pscore=document.querySelector("#pScore");
+    const cscore=document.querySelector("#cScore");
+
+    pscore.textContent=score1;
+    cscore.textContent=score2;
+}
+
+let GameOverCheck=(count1,count2)=>
+{
+    return (count1==totalRounds || count2==totalRounds);
+}
+let GameOver=(count1,count2)=>{
+
+    
+    let text;
+
+    if (count1==totalRounds)
+        text="Human Wins!!!";
+    else if (count2==totalRounds)
+        text="Computer Wins!!!";
+
+    displaymodal(text);
+
+    
+}
+
+let playRound = (playerselection) => {
+
     let computerselection = getComputerChoice();
-    return conditions(playerselection,computerselection);
-}
+    let result = conditions(playerselection, computerselection);
+    writeScore(playerCount,compCount);
+    writeSymbol(playerselection, computerselection);
+    if(GameOverCheck(playerCount,compCount))
+    {
+    GameOver(playerCount,compCount);
 
-let Game=()=>{
-    let rounds=5,playerCount=0,compCount=0;
-    for (let index = 0; index < rounds; index++) {
-        let result=playRound();
-
-        if(result==1)
-        {
-            console.log("You wins this round");
-        }
-        else if(result==-1)
-        {
-            console.log("Computer wins this round");
-        }
-        else
-        {
-            console.log("Tie");
-        }
-
-        playerCount=(result==1)?playerCount+1:playerCount;
-        compCount=(result==-1)?compCount+1:compCount;
     }
 
-    if(playerCount>compCount)
-        alert("Human Wins!!!");
-    else if(playerCount<compCount)
-        alert("Computer Wins!!!");
-    else
-        alert("Its a Tie!!");
+
+
 }
 
-Game();
+
+let displaymodal=(text)=>
+{
+    const modal=document.querySelector('#modal');
+    const heading=document.querySelector("#modal h1");
+    const btn=document.querySelector('dialog button');
+
+    btn.addEventListener('click',()=>{
+        location.reload();
+    })
+    modal.showModal();
+    heading.textContent=text;
+}
+
+if(GameOverCheck(playerCount,compCount))
+{
+GameOver(playerCount,compCount);
+}
+else
+addlisteners();
